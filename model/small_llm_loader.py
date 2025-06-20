711bbb9f7adb879259828a90a274b30b4920efd6
Suyash Gupta <23cs3064@rgipt.ac.in>
2025-06-20 10:00:52 +0530

    Update small_llm_loader.py

--- model/small_llm_loader.py
+++ model/small_llm_loader.py
@@ -2,10 +2,7 @@ from langchain_openai import ChatOpenAI
 import os
 from dotenv import load_dotenv
 
-load_dotenv()  # Loads OPENAI_API_KEY from .env
+load_dotenv()  
 
 def load_llm():
-    return ChatOpenAI(
-        model="gpt-3.5-turbo",  # or "gpt-4" if you have access
-        temperature=0.7
-    )
+    return ChatOpenAI(model="gpt-3.5-turbo",temperature=0.7)
