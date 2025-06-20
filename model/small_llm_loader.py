3300aa54ce832e5353b3d2a71952c25760ebedba
Suyash Gupta <23cs3064@rgipt.ac.in>
2025-06-20 10:44:20 +0530

    Update small_llm_loader.py

--- model/small_llm_loader.py
+++ model/small_llm_loader.py
@@ -5,4 +5,9 @@ from dotenv import load_dotenv
 load_dotenv()  
 
 def load_llm():
-    return ChatOpenAI(model="gpt-3.5-turbo",temperature=0.7)
+    openai_api_key = os.getenv("OPENAI_API_KEY")  
+    return ChatOpenAI(
+        model="gpt-3.5-turbo",
+        temperature=0.7,
+        openai_api_key=openai_api_key  
+    )
